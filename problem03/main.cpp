0815ce2471aa8a147c176f7a550ebf7a24c8b404
Lyell Haynes <lyell@sophicstudios.com>
2018-12-02 08:36:24 -0500

    Adding stubs for problem 3

---
+++ problem03/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <vector>
+#include <limits>
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        std::cerr << "missing input file in arguments!" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
